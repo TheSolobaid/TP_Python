b18a905ebf3fd2d71c736ced397d07e1b6918574
TheSolobaid <thesolobaid@gmail.com>
2022-11-22 18:08:23 +0100

    22/11/2022 17:01

--- TP3/vélo.py
+++ TP3/vélo.py
@@ -2,6 +2,8 @@ while True:
     test=0
     Total1e=0
     Total2e=0
+    nbrTemp=0
+    #test
     while test==0:
         strt = int(input("Heure de début"))
         end = int(input("Heure de fin  (toute heure entamée est due!)"))
@@ -14,18 +16,21 @@ while True:
         else:
             test=1
     nbr=(end-strt)
+    # savoir le prix dans ca commence en 1e et fini en 1e matin
     if 0<=strt<=7 and 0<=end<=7:
         total=nbr
         print("Vous avez loué votre vélo pendant:")
         print(nbr," heure(s) au tarif horaire de 1.0 euro(s)")
         print("Le montant total à payer est de ",total," euro(s)")
         break
+    # savoir le prix dans ca commence en 1e et fini en 1e soir
     elif 17<=strt<=24 and 17<=end<=24:
         total = nbr
         print("Vous avez loué votre vélo pendant:")
         print(nbr, " heure(s) au tarif horaire de 1.0 euro(s)")
         print("Le montant total à payer est de ", total, " euro(s)")
         break
+    # savoir le prix dans ca commence en 2e et fini en 2e
     elif 7<strt<17 and 7<end<17:
         total=nbr*2
         print("Vous avez loué votre vélo pendant:")
@@ -33,6 +38,7 @@ while True:
         print("Le montant total à payer est de ", total, " euro(s)")
         break
     else:
+        # savoir le prix dans ca commence en 1e et fini en 1e (avec du 2e entre)
         if strt<=7 and end>=17:
             nbrTemp=0
             for i in range(strt, 7):
@@ -40,13 +46,43 @@ while True:
                 nbrTemp=nbrTemp+1
             strt = nbrTemp + strt
             nbrTemp=0
-            for i in range(strt,18):
+            for i in range(strt,17):
                 Total2e=Total2e+1
                 nbrTemp = nbrTemp + 1
             strt = nbrTemp + strt
-            for i in range(strt,end+1):
+            for i in range(strt,end):
                 Total1e=Total1e+1
             total=Total1e+(Total2e*2)
+            print("Vous avez loué votre vélo pendant:")
+            print(Total1e, " heure(s) au tarif horaire de 1.0 euro(s)")
+            print(Total2e, " heure(s) au tarif horaire de 2.0 euro(s)")
+            print("Le montant total à payer est de ", total, " euro(s)")
+            break
+        #savoir le prix quand ca commance en 1e et fini en 2e
+        elif strt<=7 and end<17:
+            for i in range(strt, 7):
+                Total1e = Total1e + 1
+                nbrTemp = nbrTemp + 1
+            strt = nbrTemp + strt
+            nbrTemp = 0
+            for i in range(strt, end ):
+                Total2e = Total2e + 1
+            total = Total1e + (Total2e * 2)
+            print("Vous avez loué votre vélo pendant:")
+            print(Total1e, " heure(s) au tarif horaire de 1.0 euro(s)")
+            print(Total2e, " heure(s) au tarif horaire de 2.0 euro(s)")
+            print("Le montant total à payer est de ", total, " euro(s)")
+            break
+        #savoir le prix quand ca commance en 2e et fini en 1e
+        else:
+            for i in range(strt, 17):
+                Total2e = Total2e + 1
+                nbrTemp = nbrTemp + 1
+            strt = nbrTemp + strt
+            nbrTemp = 0
+            for i in range(strt, end):
+                Total1e = Total1e + 1
+            total = Total1e + (Total2e * 2)
             print("Vous avez loué votre vélo pendant:")
             print(Total1e, " heure(s) au tarif horaire de 1.0 euro(s)")
             print(Total2e, " heure(s) au tarif horaire de 2.0 euro(s)")
