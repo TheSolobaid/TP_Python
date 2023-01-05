15b1b749ae9b2a4537f8d5b2274a1255aeb5a14b
TheSolobaid <thesolobaid@gmail.com>
2023-01-05 19:15:53 +0100

    01/12/2022 09:57

--- TP5/Verification.py
+++ TP5/Verification.py
@@ -6,10 +6,10 @@ def sec2rest(seconds):
     days, hours = divmod(hours, 24)
     months, days = divmod(days, 30)
     return months, days, hours, minutes, seconds
-def find_date(seconds_since_epoch):
+def find_date(sec):
     epoch = datetime(1970, 1, 1)
-    date = epoch + timedelta(seconds=seconds_since_epoch)
-    return date.strftime("%Y-%m-%d")
+    date = epoch + timedelta(seconds=sec)
+    return date.strftime("%d-%m-%Y")
 
 while True:
     Fich1 = str(input("nom du premier fichier à chercher: "))
@@ -19,12 +19,13 @@ while True:
         print(f"la taille de {Fich1} est: {os.path.getsize(Fich1)} octet(s)")
         print(f"{Fich1} à été modifié pour la dernière fois le {find_date(os.path.getmtime(Fich1))}")
         print()
-    else:
+    elif os.path.isfile(Fich1) == False:
         print(f"{Fich1} n'existe pas")
+        print()
     if os.path.isfile(Fich2) == True:
         print(f"La taille de {Fich2} est: {os.path.getsize(Fich2)} octet(s)")
         print(f"{Fich2} à été modifié pour la dernière fois le {find_date(os.path.getmtime(Fich2))}")
         print()
-    else:
+    elif os.path.isfile(Fich2) == False:
         print(f"{Fich2} n'existe pas")
         print()
